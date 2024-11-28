6be6f0118456166dab289226835dc8f511e7c4c9
Mayowa Adegbola <writemayowa@gmail.com>
2024-11-28 02:47:00 -0500

    logs

--- index.js
+++ index.js
@@ -21,6 +21,18 @@ const limiter = rateLimit({
 });
 app.use("/search", limiter);
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 sequelize
   .authenticate()
   .then(() => {
