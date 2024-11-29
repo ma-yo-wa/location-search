efdc9c67f8f50c6052154a72f3b7b0c621a4efce
Mayowa Adegbola <writemayowa@gmail.com>
2024-11-29 02:27:54 -0500

    Improve seeder logic for csv

--- seeders/load-csv-data.js
+++ seeders/load-csv-data.js
@@ -7,34 +7,47 @@ const csvFilePath = './data/geolocation_data.csv';
 module.exports = {
   async up(queryInterface, Sequelize) {
     const records = [];
+
     return new Promise((resolve, reject) => {
-      fs.createReadStream(csvFilePath)
+      const fileStream = fs.createReadStream(csvFilePath);
+
+      fileStream
         .pipe(csvParser())
         .on('data', (row) => {
           records.push({
-            street: row.street,
-            city: row.city,
-            zip_code: row.zip_code,
-            county: row.county,
-            country: row.country,
-            latitude: parseFloat(row.latitude),
-            longitude: parseFloat(row.longitude),
-            geohash: ngeohash.encode(parseFloat(row.latitude), parseFloat(row.longitude)),
-            time_zone: row.time_zone,
+            street: row.street || null, // Use null if not provided
+            city: row.city || null,
+            zip_code: row.zip_code || null,
+            county: row.county || null,
+            country: row.country || null,
+            latitude: row.latitude ? parseFloat(row.latitude) : null,
+            longitude: row.longitude ? parseFloat(row.longitude) : null,
+            geohash: row.latitude && row.longitude ? ngeohash.encode(parseFloat(row.latitude), parseFloat(row.longitude)) : null,
+            time_zone: row.time_zone || null,
             createdAt: new Date(),
             updatedAt: new Date(),
           });
         })
         .on('end', async () => {
+          if (records.length === 0) {
+            console.warn('No records found to insert.');
+            resolve(); // Resolve even if no records
+            return;
+          }
+
           try {
-            console.log(records, 'here')
             await queryInterface.bulkInsert('Locations', records);
+            console.log('Data successfully inserted into Locations table.');
             resolve();
           } catch (error) {
+            console.error('Error inserting data into Locations table:', error.message);
             reject(error);
           }
         })
-        .on('error', (error) => reject(error));
+        .on('error', (error) => {
+          console.error('Error reading CSV file:', error.message);
+          reject(error);
+        });
     });
   },
 
